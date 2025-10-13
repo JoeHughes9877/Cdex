149dbadfd1f8f6c39b528bb48c8686034bb7a714
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-10-13 01:53:04 +0100

    removed testing code

--- db/db.py
+++ db/db.py
@@ -5,11 +5,4 @@ def open_db():
     file_path = Path(__file__).parent / "books.db"
 
     conn = sqlite3.connect(file_path)
-    cur = conn.cursor()
-
-    cur.execute('SELECT * FROM authors')
-    rows = cur.fetchall()
-    conn.close()
-
-    for row in rows:
-        print(row)
+    return conn
