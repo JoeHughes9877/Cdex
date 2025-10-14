1de71453f99f43dff1b24ab91bd90dd52981bb52
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-10-14 03:21:25 +0100

    added joins for auther/world names

--- repository/books.py
+++ repository/books.py
@@ -6,16 +6,24 @@ def get_book_by_name(q):
 
     search_term = f"%{q}%"
 
-    rows = cur.execute( ''' SELECT b.id, b.title, b.year, b.genre, a.name AS author_name FROM books b JOIN authors a ON b.author_id = a.id WHERE LOWER(b.title) LIKE LOWER(?) ''', (search_term,) ).fetchall()
+    rows = cur.execute('''
+        SELECT b.title, b.year, b.genre, a.name AS author_name, w.name AS world_name
+        FROM books b
+        JOIN authors a ON b.author_id = a.id
+        JOIN worlds w ON b.world_id = w.id
+        WHERE LOWER(b.title) LIKE LOWER(?)
+    ''', (search_term,)).fetchall()
+
     conn.close()
 
     books = []
     for row in rows:
         books.append({
-            "id": row[0],
-            "name": row[1],
-            "birth_year": row[2],
-            "nationality": row[3]
+            "name": row[0],
+            "birth_year": row[1],
+            "nationality": row[2],
+            "author_name": row[3], 
+            "world_name": row[4]
         })
     
     return books
