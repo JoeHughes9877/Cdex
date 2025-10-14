7a07be23024c17b9a1785a688f6f0bfe756290e9
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-10-14 01:47:44 +0100

    SELECT author by name

--- repository/authors.py
+++ repository/authors.py
@@ -1,4 +1,17 @@
+from db.db import open_db
+
 def get_author_by_name(name_input):
     conn = open_db()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM author')
+
+    search_term = f"%{name_input}%"
+
+    rows = cur.execute('SELECT name FROM authors WHERE LOWER(name) LIKE LOWER(?)', (search_term,)).fetchall()
+    conn.close()
+
+    authors = []
+    for row in rows:
+        authors.append(row[0])
+    
+    return authors
+
