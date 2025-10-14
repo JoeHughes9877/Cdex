fe07ac0d740e518ccc9d0106fe1830ad551e48e3
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-10-14 03:06:38 +0100

    structured and core search endpoints

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI,  Query
 from repository.authors import get_author_by_name
+from repository.books import get_book_by_name
 from typing import List, Optional 
 
 app = FastAPI()
@@ -9,3 +10,29 @@ app = FastAPI()
 async def authors( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
     data = get_author_by_name(q)
     return {"authors": data}
+
+@app.get("/books/")
+async def books( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
+    data = get_book_by_name(q)
+    return {"books": data}
+
+@app.get("/characters/")
+async def characters( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
+
+    return {"characters": data}
+
+@app.get("/kingdoms/")
+async def kingdoms( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
+
+    return {"kingdoms": data}
+
+@app.get("/quotes/")
+async def books( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
+
+    return {"books": data}
+
+@app.get("/worlds/")
+async def books( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
+
+    return {"worlds": data}
+
