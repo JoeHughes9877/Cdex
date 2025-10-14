2cfd70022f19fdb26fa71598a176046041b8eae4
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-10-14 02:51:08 +0100

    Add /authors/ GET endpoint to fetch authors with optional search

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,  Query
 from repository.authors import get_author_by_name
+from typing import List, Optional 
 
 app = FastAPI()
 
-@app.get("/")
-def root():
- 
-    data = get_author_by_name("g")
 
+@app.get("/authors/")
+async def authors( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
+    data = get_author_by_name(q)
     return {"authors": data}
