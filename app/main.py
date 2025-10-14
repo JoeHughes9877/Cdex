37485116a955a13c60751e8848fea7604ddc04a5
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-10-14 21:43:54 +0100

    added and removed links based on new schema

--- app/main.py
+++ app/main.py
@@ -1,38 +1,34 @@
 from fastapi import FastAPI,  Query
-from repository.authors import get_author_by_name
-from repository.books import get_book_by_name
+from repository import authors, books, characters, worlds, series
+
+
 from typing import List, Optional 
 
 app = FastAPI()
 
-
 @app.get("/authors/")
 async def authors( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
-    data = get_author_by_name(q)
+    data = authors.get_author_by_name(q)
     return {"authors": data}
 
 @app.get("/books/")
 async def books( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
-    data = get_book_by_name(q)
+    data = books.get_book_by_title(q)
     return {"books": data}
 
 @app.get("/characters/")
 async def characters( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
-
+    data = characters.get_character_by_name(q)
     return {"characters": data}
 
-@app.get("/kingdoms/")
-async def kingdoms( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
-
-    return {"kingdoms": data}
-
-@app.get("/quotes/")
-async def books( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
-
-    return {"books": data}
-
 @app.get("/worlds/")
-async def books( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
-
+async def worlds( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
+    data = worlds.get_world_by_name(q)
     return {"worlds": data}
+
+@app.get("/series/")
+async def worlds( skip: int = 0, limit: int = 10, q: Optional[str] = Query(None)):
+    data = series.get_series_by_name(q)
+    return {"series": data}
+
 
