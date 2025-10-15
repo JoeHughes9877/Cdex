c5374749f0f5a4dd4f8d3dac9d1a4caff4937e0b
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-10-15 02:52:10 +0100

    added opening, and session creation for database

--- db/db.py
+++ db/db.py
@@ -1,8 +1,21 @@
-import sqlite3
-from pathlib import Path
+from typing import Annotated
 
-def open_db():
-    file_path = Path(__file__).parent / "books.db"
+from fastapi import Depends
+from sqlmodel import Session, SQLModel, create_engine
 
-    conn = sqlite3.connect(file_path)
-    return conn
+
+sqlite_file_name = "db/books.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
