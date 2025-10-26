7b7c8465264717624b8b59c63bdd2b66af5a89db
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-10-26 01:17:37 +0000

    api key auth

--- app/auth.py
+++ app/auth.py
@@ -1,12 +1,10 @@
+# app/auth.py
 import secrets
 import hashlib
-from db.db import SessionDep
+from fastapi import Depends, HTTPException, status
+from sqlmodel import Session, select
 from app.models import APIKey
-from typing import Optional
-
-from app.models import APIKey
-import secrets, hashlib
-from sqlmodel import Session
+from db.db import SessionDep  
 
 def create_read_only_key(session: Session):
     key = secrets.token_urlsafe(16)
@@ -20,9 +18,20 @@ def create_read_only_key(session: Session):
 
     session.add(api_key_instance)
     session.commit()
-    session.refresh(api_key_instance) 
-
+    session.refresh(api_key_instance)
     return key
 
-def verify_key(key: str, session: Session) -> Optional[APIKey]:
-    ...
+
+def api_key_auth(key: str, session: Session = Depends(SessionDep)):
+    hashed_key = hashlib.sha256(key.encode()).hexdigest()
+
+    stmt = select(APIKey).where(APIKey.api_key_hash == hashed_key)
+    found = session.exec(stmt).first()
+
+    if not found:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API key",
+        )
+
+    return True
